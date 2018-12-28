1ea5170c7a9391c712bf9b83ea8e9ead5b308730
victor.aguilar <victor.aguilarsnz@gmail.com>
2018-12-28 15:34:23 -0600

    Bootstrap installed

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -1,4 +1,10 @@
 class Habit < ApplicationRecord
   has_many :activities
   validates :title, presence: true, length: { minimum: 3, maximum: 50 }
+  before_destroy :destroy_activities
+
+  private
+  def destroy_activities
+    self.activities.destroy_all
+  end
 end
