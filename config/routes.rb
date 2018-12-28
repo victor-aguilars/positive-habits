07a982d6c3e3475e944decf35bb16c421896f85e
victor.aguilar <victor.aguilarsnz@gmail.com>
2018-12-28 15:41:00 -0600

    Added homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
+  root 'home#index'
+  
   resources :activities
   resources :habits
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 end
