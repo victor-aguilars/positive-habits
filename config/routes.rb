babb65d7e5149eb6305ddce69747da194abcef91
victor.aguilar <victor.aguilarsnz@gmail.com>
2018-12-29 11:06:07 -0600

    New home

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  root 'home#index'
-  
+  root 'habits#index'
+
   resources :activities
   resources :habits
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
